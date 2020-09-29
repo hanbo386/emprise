3fffb004c68dd6675c4cb2bd1d457a57c0dc9d48
bhan <bo.han@trademe.co.nz>
2020-09-29 15:27:01 +1300

    refactor attack mode

--- src/core/app-store/position/position-state.model.ts
+++
@@ -1,3 +0,0 @@
-export interface PositionState {
-  heroPositions: any;
-}
--- src/core/app-store/position/position.reducer.ts
+++
@@ -1,7 +0,0 @@
-import { PositionState } from '../position-state.model';
-import { Action } from '../../rx/models';
-
-export function positionReducer(state: PositionState, action: Action): PositionState {
-  const newPosition:[number, number] = [state.position[0] + 1, state.position[1] + 1];
-  return {position: newPosition};
-}
